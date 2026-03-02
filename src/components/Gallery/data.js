998f889aa69007648998e824acfcde86cdd95a70
Makar <kolesnykmakar0@gmail.com>
2026-03-02 23:17:58 +0200

    Add gallery

---
+++ src/components/Gallery/data.js
@@ -0,0 +1,24 @@
+const arrayPics = [
+  {
+    id: 1,
+    src: "https://static.toiimg.com/thumb/122784728.jpg?imgsize=70140&photoid=122784728&width=600&height=335&resizemode=75",
+    alt: "sea",
+  },
+  {
+    id: 2,
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/500px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg",
+    alt: "mountin",
+  },
+  {
+    id: 3,
+    src: "https://data.tibettravel.org/assets/images/tour-review/august-review/tour-review-august-mount-everest-theme-pic.jpg",
+    alt: "everest",
+  },
+  {
+    id: 4,
+    src: "https://cdn.britannica.com/16/60016-004-B7128B50.jpg",
+    alt: "island",
+  },
+];
+
+export default arrayPics;
---
+++ src/components/Picture/Picture.jsx
@@ -0,0 +1,8 @@
+import styles from "./Picture.module.css"
+
+const Picture = (props) => {
+  const { src, alt } = props;
+  return <img className={styles.img} src={src} alt={alt} />;
+};
+
+export default Picture;
