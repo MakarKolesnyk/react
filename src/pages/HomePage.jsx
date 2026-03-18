49674fafab4db64cd29fbb7d3917ada6ec9d0887
Makar <kolesnykmakar0@gmail.com>
2026-03-18 18:46:56 +0200

    Add redux-toolkit & counter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Counter from "../components/Counter/Counter";
 
 
 const HomePage = () => {
     return (
         <section>
             <h1>Home Page</h1>
+            <Counter/>
         </section>
     );
 }
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSliceReducer from './counterSlice'
+const store = configureStore({
+    reducer: {
+        counter: counterSliceReducer
+    }
+})
+
+export default store
